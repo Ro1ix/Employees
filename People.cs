6288e105138d8ea8d9469d472a4b4b0d8c845dd6
Ro1ix <pitonvano05@gmail.com>
2023-12-21 11:36:29 +0300

    v0.1 Beta

--- People.cs
+++ People.cs
@@ -8,6 +8,32 @@ namespace Employees
 {
     internal class People
     {
-        
+        private string name;
+        private int age;
+        public void Start()
+        {
+            InfoIn();
+        }
+        private void InfoIn()
+        {
+            string input;
+            Console.Write("Имя: ");
+            name = Console.ReadLine();
+            Console.Write("Возраст: ");
+            do
+            {
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out age) == false)
+                {
+                    Console.WriteLine("ОШИБКА!!! Возраст должен состоять только из цифр\nНажмите Enter и попробуйте ещё раз");
+                    do
+                    {
+                        //Nothing
+                    } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+
+                }
+            } while (Int32.TryParse(input, out age) == false);
+            if 
+        }
     }
 }
