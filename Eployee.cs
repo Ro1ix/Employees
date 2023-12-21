ae8344c2b00e44342325e73cda4734a6de38e453
Ro1ix <pitonvano05@gmail.com>
2023-12-21 16:24:22 +0300

    v0.2 Beta

--- Eployee.cs
+++ Eployee.cs
@@ -6,8 +6,51 @@ using System.Threading.Tasks;
 
 namespace Employees
 {
-    internal class Eployee
+    internal class Eployee : People
     {
+        protected int section;
+        protected int workDay = 0;
+        protected int workNight = 0;
+        protected double salary;
+        protected override void InfoIn()
+        {
+            base.InfoIn();
+            string input;
+            do
+            {
+                Console.WriteLine("Введите отдел (1 или 2): ");
+                input = Console.ReadLine();
+                if (input == "1")
+                {
+                    section = 1;
+                }
+                else if (input == "2")
+                {
+                    section = 2;
+                }
+                else
+                {
+                    Console.WriteLine("ОШИБКА!!! Такого отдела не существует\nНажмите Enter и попробуйте ещё раз . . .");
+                    do
+                    {
+                        //Nothing
+                    } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                    Console.Clear();
+                    Console.WriteLine($"Имя: {name}");
+                    Console.WriteLine($"Возраст (от 16 до 50): {age}");
+                }
+            } while (section != 1 && section != 2);
+        }
+        protected void Work()
+        {
+            Random random = new Random();
+            workDay = random.Next(1, 31);
+            if (age >= 18)
+            {
+                workNight = 30 - workDay;
+            }
+            salary = workDay * 2000 + workNight * 3500;    /*2000 - зарплата за одну дневную смену, 3500 - за одну ночную*/
 
+        }
     }
 }
--- People.cs
+++ People.cs
@@ -8,32 +8,41 @@ namespace Employees
 {
     internal class People
     {
-        private string name;
-        private int age;
-        public void Start()
-        {
-            InfoIn();
-        }
-        private void InfoIn()
+        protected string name;
+        protected int age;
+        protected virtual void InfoIn()
         {
             string input;
             Console.Write("Имя: ");
             name = Console.ReadLine();
-            Console.Write("Возраст: ");
             do
             {
+                Console.Write("Возраст (от 16 до 50): ");
                 input = Console.ReadLine();
                 if (Int32.TryParse(input, out age) == false)
                 {
-                    Console.WriteLine("ОШИБКА!!! Возраст должен состоять только из цифр\nНажмите Enter и попробуйте ещё раз");
+                    Console.WriteLine("ОШИБКА!!! Возраст должен состоять только из цифр\nНажмите Enter и попробуйте ещё раз . . .");
                     do
                     {
                         //Nothing
                     } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
-
+                    Console.Clear();
+                    Console.WriteLine($"Имя: {name}");
                 }
-            } while (Int32.TryParse(input, out age) == false);
-            if 
+                else
+                {
+                    if (age < 16 || age > 50)
+                    {
+                        Console.WriteLine("ОШИБКА!!! Возраст должен быть от 16 до 50 лет\nНажмите Enter и попробуйте ещё раз . . .");
+                        do
+                        {
+                            //Nothing
+                        } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                        Console.Clear();
+                        Console.WriteLine($"Имя: {name}");
+                    }
+                }
+            } while (Int32.TryParse(input, out age) == false || age < 16 || age > 50);
         }
     }
 }
